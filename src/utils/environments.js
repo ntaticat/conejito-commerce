07a9e86fc988c9cc1e7d91a453f62b9a66df1978
Rafael Estrada <ntaticat@gmail.com>
2022-07-28 13:07:57 -0500

    update environment variables

--- src/utils/environments.js
+++ src/utils/environments.js
@@ -1,4 +1,1 @@
-export const apiUrl = process.env.NODE_ENV === "development" || 
-                      process.env.NODE_ENV === "test" ? 
-                      process.env.REACT_APP_CONEJITOCOMMERCEAPI_DEV : 
-                      process.env.REACT_APP_CONEJITOCOMMERCEAPI_PROD;
+export const apiUrl = process.env.REACT_APP_CONEJITOCOMMERCEAPI;
